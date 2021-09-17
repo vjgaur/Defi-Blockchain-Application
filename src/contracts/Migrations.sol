ef1a25e44da296bad709d22c992ada812bd22d14
vjgaur <vijayendra.gaur@gmail.com>
2021-09-17 22:53:07 +0530

    added migration and address upgrade script

---
+++ src/contracts/Migrations.sol
@@ -0,0 +1,23 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Migrations {
+    address public owner;
+    uint256 public last_completed_migration;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier restricted() {
+        if (msg.sender == owner) _;
+    }
+
+    function set_completed(uint256 completed) public restricted {
+        last_completed_migration = completed;
+    }
+
+    function upgrade(address new_address) public restricted {
+        Migrations upgraded = Migrations(new_address);
+        upgraded.set_completed(last_completed_migration);
+    }
+}
