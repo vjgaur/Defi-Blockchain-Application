f8610bbeab8fdf003fd82e38d5f4fc32829d59de
vjgaur <vijayendra.gaur@gmail.com>
2021-09-18 18:08:49 +0530

    decentralised banking peer to peer fund transfer

--- src/contracts/DecentralBank.sol
+++ src/contracts/DecentralBank.sol
@@ -1,5 +1,6 @@
 pragma solidity ^0.5.0;
 
+//Decentralised Bank
 contract DecentralBank {
     string public name = "Decentralised Bank";
     address public owner;
--- src/contracts/Reward.sol
+++ src/contracts/Reward.sol
@@ -5,7 +5,7 @@ contract Reward {
     string public symbol = "RWD";
     uint256 public totalSupply = 1000000000000000000000000; // 1 million tokens
     uint8 public decimals = 18;
-
+    
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
     event Approval(
